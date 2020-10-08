3bcc6e9163f6db24f716d265534f06ddcc9dee0a
Diego Enriquez-Serrano <diego@Diegos-MacBook-Pro.local>
2020-10-08 07:50:29 -0500

    fix product model test and create cart scaffold

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,6 +9,7 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:title].any?
     assert product.errors[:description].any?
     assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
   end
 
   test "product price must be positive" do
@@ -37,18 +38,23 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.jpg
-             http://a.b.c/x/y/z/fred.gif.more }
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
     ok.each do |image_url|
-      assert new_product(image_url).valid?
+      assert new_product(image_url).valid?,
         "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+        "#{image_url} shouldn't be valid"
     end
   end
 
   test "product is not valid without a unique title" do
-    product = Product.new(title:    products(:ruby).title,
+    product = Product.new(title: products(:ruby).title,
                           description: 'yyy',
                           price: 1,
                           image_url: "fred.gif")
