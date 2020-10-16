3091e3af2b23e135e7d28e1c92859950671f8485
Diego Enriquez-Serrano <diego@Diegos-MacBook-Pro.local>
2020-10-16 08:00:33 -0500

    create cart sessions and line item relations

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -5,7 +5,7 @@ module CurrentCart
     def set_cart
       @cart = Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
-      @cart = Cart.create_with
+      @cart = Cart.create
       session[:cart_id] = @cart.id
     end
 end
